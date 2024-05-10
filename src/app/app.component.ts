3efde1d86d8c84d61203281d4579884e392f99d6
emielh <e.hertogs@anva.nl>
2024-05-10 08:56:39 +0200

    basic layout for future exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
+import {DummyComponent} from "./dummy.component";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
 
 interface ComponentInterface {
@@ -11,16 +13,20 @@ interface ComponentInterface {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSelect, MatOption],
+  imports: [RouterOutlet, MatFormField, MatSelect, MatOption, MatLabel],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'design-patterns-angular';
-  protected components: ComponentInterface[] = []
+  protected components: ComponentInterface[] = [
+    {name: 'Dummy exercise', component: DummyComponent, path: 'dummy-exercise'},
+  ]
   private readonly router: Router = inject(Router);
 
   protected navigateToComponent(component: ComponentInterface): void {
+    console.log(component);
+    
     this.router.navigate([component.path]);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dummy-exercise',
+    loadComponent: () => import('./dummy.component').then(c => c.DummyComponent)
+  },
+];

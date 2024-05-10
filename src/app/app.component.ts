c86b61f041d3aa7a02a86f86bc82cb0527b312af
emielh <e.hertogs@anva.nl>
2024-05-10 07:58:24 +0200

    basic layout for future exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatOption, MatSelect} from "@angular/material/select";
+
+interface ComponentInterface {
+  name: string;
+  component: any;
+  path: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSelect, MatOption],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'design-patterns-angular';
+  protected components: ComponentInterface[] = []
+  private readonly router: Router = inject(Router);
+
+  protected navigateToComponent(component: ComponentInterface): void {
+    this.router.navigate([component.path]);
+  }
 }

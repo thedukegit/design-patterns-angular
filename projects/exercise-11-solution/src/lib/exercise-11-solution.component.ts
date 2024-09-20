e5bc7702e932fe519121fd2c01fd104f7e7b8cdb
emielh <e.hertogs@anva.nl>
2024-09-20 07:54:28 +0200

    implemented a history of saved states

--- projects/exercise-11-solution/src/lib/exercise-11-solution.component.ts
+++ projects/exercise-11-solution/src/lib/exercise-11-solution.component.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core';
 import {Ship} from "./models/ship";
 import {JsonPipe} from "@angular/common";
 import {Memento} from "./models/memento";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'lib-exercise-11-solution',
   standalone: true,
   imports: [
-    JsonPipe
+    JsonPipe,
+    MatButton
   ],
   templateUrl: './exercise-11-solution.component.html',
   styles: ``
@@ -16,7 +18,7 @@ import {Memento} from "./models/memento";
 export class Exercise11SolutionComponent {
   protected myShip: Ship | null;
 
-  protected saveState: Memento = {} as Memento; // here it holds the Memento object
+  protected savedStates: Array<Memento> = []; // here it holds the Memento objects
 
   public constructor() {
     this.myShip = new Ship();
@@ -30,15 +32,15 @@ export class Exercise11SolutionComponent {
 
   public save(): void {
     if (this.myShip)
-      this.saveState = this.myShip.createMemento() // here it saves the state
+      this.savedStates.push(this.myShip.createMemento()) // here it saves the state
   }
 
   public destroy(): void {
     this.myShip = null;
   }
 
-  public reload(): void {
+  public reload(index: number): void {
     if (!this.myShip) this.myShip = new Ship(); // recreate the ship if its destroyed
-    this.myShip.shields = this.saveState.getState(); // here it restores the state
+    this.myShip.shields = this.savedStates[index].getState(); // here it restores the state
   }
 }
